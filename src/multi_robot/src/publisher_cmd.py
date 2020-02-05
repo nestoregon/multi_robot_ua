62087c0460cd50c52aa15c4eb20dc5d8a2b68b03
nestoregon <nestormorales1998@gmail.com>
2020-02-05 21:49:42 +0100

    Launch file now includes 4 robots

--- src/multi_robot/src/publisher_cmd.py
+++ src/multi_robot/src/publisher_cmd.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 
 import rospy
+import sys
 from geometry_msgs.msg import Twist
 
 def movimiento():
   vel = Twist()
   vel.linear.x = 0.5
   vel.angular.z = 0.3
-  pub1.publish(vel)
+  pub.publish(vel)
 
-
+topic = str(sys.argv[1]) + '/cmd_vel'
 rospy.init_node('publisher_cmd', anonymous=True)
-pub1 = rospy.Publisher('robot2/cmd_vel', Twist, queue_size =5)
+pub = rospy.Publisher(topic, Twist, queue_size =5)
 rate = rospy.Rate(10) # 0.1 segundo
 
 while(1):
