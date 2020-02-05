719ce8b63ca8c434fc4ea9e7b7723459a432c4fd
nestoregon <nestormorales1998@gmail.com>
2020-02-05 21:18:08 +0100

    Changes

--- src/multi_robot/src/publisher_cmd.py
+++ src/multi_robot/src/publisher_cmd.py
@@ -7,16 +7,13 @@ def movimiento():
   vel = Twist()
   vel.linear.x = 0.5
   vel.angular.z = 0.3
-
   pub1.publish(vel)
-  pub2.publish(vel)
 
 
-rospy.init_node('argu/publisher', anonymous=False)
-pub1 = rospy.Publisher('argu/mobile_base/commands/velocity', Twist, queue_size =5)
+rospy.init_node('publisher_cmd', anonymous=True)
+pub1 = rospy.Publisher('robot2/cmd_vel', Twist, queue_size =5)
 rate = rospy.Rate(10) # 0.1 segundo
 
 while(1):
   movimiento()
   rate.sleep()
-
