7b44900c3f98d65f1a71a664033df1aaed52c4a3
nestoregon <nestormorales1998@gmail.com>
2020-02-07 14:19:38 +0100

    Changes to py

--- src/multi_robot/src/publisher_cmd.py
+++ src/multi_robot/src/publisher_cmd.py
@@ -2,24 +2,58 @@
 
 import rospy
 import sys
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Pose
+from nav_msgs.msg import Odometry
 
-def movimiento():
-  vel = Twist()
-  vel.linear.x = 0.5
-  vel.angular.z = 0.3
-  pub.publish(vel)
 
-topic = str(sys.argv[1]) + '/cmd_vel'
-rospy.init_node('publisher_cmd', anonymous=True)
-pub = rospy.Publisher(topic, Twist, queue_size =5)
-rate = rospy.Rate(10) # 0.1 segundo
+"""
+Definir un movimiento
+"""
+class square_movement():
+  def __init__(self, x=0, z=0):
+    """Initialize node"""
+    rospy.init_node('publisher_cmd', anonymous= True)
+    self.odom = Odometry()
+    # self.pose = Pose()
+    self.vel = Twist()
+    self.vel.linear.x = x
+    self.vel.angular.z = z
+    self.pub_topic_name = str(sys.argv[1]) + '/cmd_vel'
+    self.sub_topic_name = str(sys.argv[1]) + '/odom'
+    
+    """Create publishers y subscribers"""
+    self.pub = rospy.Publisher(self.pub_topic_name, Twist, queue_size = 5)
+    self.sub = rospy.Subscriber(self.sub_topic_name, Odometry, self.callback)
+    self.rate = rospy.Rate(5) # 0.2 segundos
 
+  def callback(self, msg):
+    # do something
+    self.odom.pose.pose.orientation = msg.pose.pose.orientation
+
+  def avanzar(self):
+    print("hola")
+
+  def giro(self):
+    self.pub.publish(self.vel)
+
+  def move(self):
+
+    self.vel.angular.z = 0.2
+    while 1:
+      self.giro()
+      print(self.odom.pose.pose.orientation)
+      print("----------------")
+      self.rate.sleep()
+    
 
 def main():
-  while(1):
-    movimiento()
-    rate.sleep()
+  movement = square_movement()
+  movement.move()
+  rospy.spin()
 
 if __name__ == "__main__":
+  try:
     main()
+  except rospy.ROSInterruptException:
+    print("Shutting down publisher")
+    pass
